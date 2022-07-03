85bb869066f76b4d2d4ded79408362058dba7723
井上真悠子 <inouemayuko@inouemayukonoMacBook-Pro.local>
2022-07-03 12:15:54 +0900

    add error message when creating user

--- app/src/main/java/calendar/reserve/app/controllers/UserController.java
+++ app/src/main/java/calendar/reserve/app/controllers/UserController.java
@@ -3,6 +3,7 @@ package calendar.reserve.app.controllers;
 import calendar.reserve.app.services.UserService;
 import calendar.reserve.app.services.JsonTransformer;
 import calendar.reserve.app.models.User;
+import calendar.reserve.app.models.ErrorMessage;
 
 import static spark.Spark.*;
 
@@ -17,6 +18,7 @@ public class UserController {
             post("/signup", (req, res) -> {
                 try {
                     String json = req.body();
+                    res.type("application/json");
                     User user = jsonTransformer.fromJson(json, User.class);
                     return userService.create(user);
                 } catch (Exception e) {
@@ -24,10 +26,13 @@ public class UserController {
                 }
             }, jsonTransformer);
 
-            get("/hello", (req, res) -> "Hello World");
+            get("/hello", "application/json", (req, res) -> "Hello World");
 
             exception(Exception.class, (exception, request, response) -> {
-                System.out.println(exception.getMessage());
+                response.type("application/json");
+                String responseMessage = "{\"message\":" + exception.getMessage() + "}";
+                response.status(400);
+                response.body(responseMessage);
             });
         } catch (Exception e) {
             System.out.println(e.getMessage());
---
+++ app/src/main/java/calendar/reserve/app/models/ErrorMessage.java
@@ -0,0 +1,25 @@
+package calendar.reserve.app.models;
+
+import java.io.Serializable;
+
+public class ErrorMessage implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+	public static final String MESSAGE = "message";
+	
+
+    private String message;
+   
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+ 
+    public void SetMessage(String message) {
+        this.message = message;
+    }
+}
